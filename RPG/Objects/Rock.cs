2184dd96a4d0d4e40c30a7ef9d244a1919692178
Cooper Saye <coopersaye@gmail.com>
2021-04-01 12:35:12 -0600

    Create Rock object.

---
+++ RPG/Objects/Rock.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace RPG.Objects
+{
+    public class Rock : GameObject
+    {
+        private Rectangle Bounds
+        {
+            get { return new Rectangle(X, Y, W, H); }
+        }
+
+        public Rock(int x, int y) : base(x, y, 32, 32) {}
+
+        public override void Draw(Game1 game)
+        {
+            Drawing.DrawRect(Bounds, Color.Gray, game);
+        }
+    }
+}
