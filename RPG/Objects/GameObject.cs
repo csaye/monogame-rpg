6769c6947678e8d70dfa57adbf9d034d0bcdc132
Cooper Saye <coopersaye@gmail.com>
2021-04-01 12:34:25 -0600

    Create GameObject class.

---
+++ RPG/Objects/GameObject.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace RPG.Objects
+{
+    public abstract class GameObject
+    {
+        // Position
+        public int X { get; private set; }
+        public int Y { get; private set; }
+
+        // Size
+        public int W { get; private set; }
+        public int H { get; private set; }
+
+        public GameObject(int x, int y, int w, int h)
+        {
+            X = x;
+            Y = y;
+            W = w;
+            H = h;
+        }
+
+        public abstract void Draw(Game1 game);
+    }
+}
