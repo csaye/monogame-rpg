991eed5b162900007923165560b021e64b685efb
Cooper Saye <coopersaye@gmail.com>
2021-04-01 14:06:33 -0600

    Implement sorting layers.

--- RPG/Drawing.cs
+++ RPG/Drawing.cs
@@ -5,11 +5,11 @@ namespace RPG
 {
     public static class Drawing
     {
-        public static void DrawRect(Rectangle rect, Color color, Game1 game)
+        public static void DrawRect(Rectangle rect, Color color, Game1 game, float depth)
         {
             Texture2D texture = new Texture2D(game.GraphicsDevice, 1, 1);
             texture.SetData(new[] { Color.White });
-            game.SpriteBatch.Draw(texture, rect, color);
+            game.SpriteBatch.Draw(texture, rect, null, color, 0, new Vector2(0, 0), SpriteEffects.None, depth);
         }
     }
 }
---
+++ RPG/Utility/SortingLayers.cs
@@ -0,0 +1,8 @@
+﻿namespace RPG.Utility
+{
+    public static class SortingLayers
+    {
+        public const float Objects = 1;
+        public const float Player = 0;
+    }
+}
