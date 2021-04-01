1d46bfed57051b876eb33d21e859d4bc21b0a396
Cooper Saye <coopersaye@gmail.com>
2021-04-01 12:34:39 -0600

    Create ObjectManager class.

---
+++ RPG/Objects/ObjectManager.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Xna.Framework;
+using System.Collections.Generic;
+
+namespace RPG.Objects
+{
+    public class ObjectManager
+    {
+        private List<GameObject> gameObjects = new List<GameObject>();
+
+        public ObjectManager() {}
+
+        public void Draw(Game1 game)
+        {
+            foreach (GameObject obj in gameObjects)
+            {
+                obj.Draw(game);
+            }
+        }
+    }
+}
