6515284535a4fae2af41513c32bebbf29e63c3ae
Cooper Saye <coopersaye@gmail.com>
2021-04-02 17:59:03 -0600

    Implement camera.

---
+++ RPG/Camera.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Xna.Framework;
+using RPG.Objects;
+
+namespace RPG
+{
+    public class Camera
+    {
+        public GameObject Target { get; set; }
+        public Matrix Transform { get; private set; }
+
+        public Camera(GameObject target)
+        {
+            Target = target;
+        }
+
+        public void Update(GameTime gameTime, Game1 game)
+        {
+            Matrix position = Matrix.CreateTranslation(
+                -Target.X - (Target.Width / 2),
+                -Target.Y - (Target.Height / 2),
+                0
+            );
+
+            Matrix offset = Matrix.CreateTranslation(
+                Drawing.Width / 2,
+                Drawing.Height / 2,
+                0
+            );
+
+            Transform = position * offset;
+        }
+    }
+}
--- RPG/Scenes/Main.cs
+++ RPG/Scenes/Main.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 using RPG.Objects;
 using RPG.Tiles;
 
@@ -6,6 +7,8 @@ namespace RPG.Scenes
 {
     public class Main : Scene
     {
+        private Camera camera;
+
         private int[,] tiles = new int[,]
         {
             {2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2},
@@ -39,7 +42,31 @@ namespace RPG.Scenes
 
             // Initialize objects
             ObjectManager.Add(new Rock(Drawing.Grid * 2, Drawing.Grid * 2));
-            ObjectManager.Add(new Player(0, 0));
+
+            // Initialize player and camera
+            Player player = new Player(0, 0);
+            ObjectManager.Add(player);
+            camera = new Camera(player);
+        }
+
+        public override void Draw(Game1 game)
+        {
+            game.SpriteBatch.Begin(SpriteSortMode.BackToFront, null, SamplerState.PointClamp, transformMatrix: camera.Transform);
+
+            // Draw tiles
+            TileManager.Draw(game);
+
+            // Draw objects
+            ObjectManager.Draw(game);
+
+            game.SpriteBatch.End();
+        }
+
+        public override void Update(GameTime gameTime, Game1 game)
+        {
+            camera.Update(gameTime, game);
+
+            base.Update(gameTime, game);
         }
     }
 }
