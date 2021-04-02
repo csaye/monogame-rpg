9c8b2682e2ba42d3525b015c518edaacdd878cc9
Cooper Saye <coopersaye@gmail.com>
2021-04-01 20:41:22 -0600

    Create TileManager class.

--- RPG/Scenes/Scene.cs
+++ RPG/Scenes/Scene.cs
@@ -1,16 +1,21 @@
 ﻿using Microsoft.Xna.Framework;
 using RPG.Objects;
+using RPG.Tiles;
 
 namespace RPG.Scenes
 {
     public abstract class Scene
     {
         public ObjectManager ObjectManager { get; } = new ObjectManager();
+        public TileManager TileManager { get; } = new TileManager();
 
         public Scene() {}
 
         public virtual void Draw(Game1 game)
         {
+            // Draw tiles
+            TileManager.Draw(game);
+
             // Draw objects
             ObjectManager.Draw(game);
         }
---
+++ RPG/Tiles/TileManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace RPG.Tiles
+{
+    public class TileManager
+    {
+        public List<Tile> Tiles { get; set; } = new List<Tile>();
+
+        public TileManager() {}
+
+        public void Draw(Game1 game)
+        {
+            foreach (Tile tile in Tiles)
+            {
+                tile.Draw(game);
+            }
+        }
+
+        public void Add(Tile tile) => Tiles.Add(tile);
+    }
+}
